81ae529ea86d2b0db70e5b6aed932991591dddc3
ste-gk <entwicklung@ste-gk.de>
2017-02-25 01:03:04 +0100

    Add files

---
+++ Household/Models/TestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Household.Models
+{
+	public class TestModel
+	{
+		public long Id { get; set; }
+		public string Text { get; set; }
+	}
+}
